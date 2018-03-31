ccc677600894fbccd89452c544a56758e9243412
Svetlana <sv.soboleva@list.ru>
2018-03-31 22:38:51 +0300

    heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,11 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://justgames.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 
+    'http://justgames.herokuapp.com/cable', 
+    'https://justgames.herokuapp.com/cable' 
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
